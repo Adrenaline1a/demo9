f27e21b8c9498fadfb68d326a8379834411a77f0
Not a Hero <78856293+Adrenaline1a@users.noreply.github.com>
2021-09-20 09:51:47 +0300

    отчёт

--- задания/инд.py
+++ задания/инд.py
@@ -1,8 +1,9 @@
+# X = (A\B) /\ ( C u D)     Y = (A /\ not(B)) u (C\D)
 if __name__ == "__main__":
     U = set("abcdefghijklmnopqrstuvwxyz")
     A = {'c', 'e', 'h', 'n'}
     B = {'e', 'f', 'k', 'n', 'x'}
     C = {'b', 'c', 'h', 'p', 'r', 's'}
     D = {'b', 'e', 'g'}
-    print((A - B) & (C | D))
-    print((C - D) | (A & (U - B)))
+    print('X =', (A - B) & (C | D))
+    print('Y =', (C - D) | (A & (U - B)))
