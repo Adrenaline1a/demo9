9473834b930a5ae8f977ba12869cf595d7d5ab0b
Not a Hero <78856293+Adrenaline1a@users.noreply.github.com>
2021-09-20 17:26:59 +0300

    pep8

--- zadanie/1.py
+++ zadanie/1.py
@@ -1,10 +1,14 @@
-# Подсчитайте количество гласных в строке, введенной
-# с клавиатуры с использованием множеств.
-if __name__ == "__main__":
-    words = {'а', 'у', 'е', 'о', 'э', 'я', 'и', 'ю', 'ё', 'ы'}
-    string = list(input('Введите строку: ').lower())
-    count = 0
-    for i, slov in enumerate(string):
-        if slov in words:
-            count += 1
-    print(count)
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+    # Подсчитайте количество гласных в строке, введенной
+    # с клавиатуры с использованием множеств.
+    if __name__ == "__main__":
+        words = {'а', 'у', 'е', 'о', 'э', 'я', 'и', 'ю', 'ё', 'ы'}
+        string = list(input('Введите строку: ').lower())
+        count = 0
+        for i, slov in enumerate(string):
+            if slov in words:
+                count += 1
+        print(count)
--- zadanie/ind.py
+++ zadanie/ind.py
@@ -1,9 +1,13 @@
-# X = (A\B) /\ ( C u D)     Y = (A /\ not(B)) u (C\D)
-if __name__ == "__main__":
-    U = set("abcdefghijklmnopqrstuvwxyz")
-    A = {'c', 'e', 'h', 'n'}
-    B = {'e', 'f', 'k', 'n', 'x'}
-    C = {'b', 'c', 'h', 'p', 'r', 's'}
-    D = {'b', 'e', 'g'}
-    print('X =', (A - B) & (C | D))
-    print('Y =', (C - D) | (A & (U - B)))
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+    # X = (A\B) /\ ( C u D)     Y = (A /\ not(B)) u (C\D)
+    if __name__ == "__main__":
+        U = set("abcdefghijklmnopqrstuvwxyz")
+        A = {'c', 'e', 'h', 'n'}
+        B = {'e', 'f', 'k', 'n', 'x'}
+        C = {'b', 'c', 'h', 'p', 'r', 's'}
+        D = {'b', 'e', 'g'}
+        print('X =', (A - B) & (C | D))
+        print('Y =', (C - D) | (A & (U - B)))
